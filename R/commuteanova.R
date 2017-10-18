af6319c6bd08380aa62468ff2644a03011ae0b58
David Burton <davids.burton@gmail.com>
2017-10-18 10:23:52 -0400

    Update commuteanova.R
    Add normality tests, homogeneity of variance tests, log transform and give results accordingly.

--- R/commuteanova.R
+++ R/commuteanova.R
@@ -1,8 +1,7 @@
 #This function will do the anova, and
 #it will display the results.
-
 commuteanova <- function(commutedf,Leave ="Left",Arrive="ArriveDesk",Time ="Total",
-                         Group="Day",timebins = 6){
+                         Group="Day",usertimebins = 6){
 
 
   if(missing(Leave) | missing(Arrive)) {
@@ -13,19 +12,22 @@ commuteanova <- function(commutedf,Leave ="Left",Arrive="ArriveDesk",Time ="Tota
 
     Arrive <- commutedf[,Arrive][[1]] %>% as.POSIXct(format = "%H:%M:%S")
     Depart <- commutedf[,Leave][[1]] %>% as.POSIXct(format = "%H:%M:%S")
+
+
     driveTime <- difftime(Arrive,Depart,units= "mins") %>% as.numeric()
 
     commutedf <- data.frame(driveTime,commutedf %>% dplyr::select(Group,Leave) %>% unclass())
 
     #kmeans cluster by length of drive to compare to time left
+    timebins <- usertimebins
     Expanse <- difftime(Depart,min(Depart),units="mins") %>% as.numeric()
     clusterdf <- driveTime %>% data.frame(Expanse)
-    timecluster <- clusterdf %>% kmeans(timebins)
-    commutedf <- commutedf %>% dplyr::mutate(Cluster = timecluster[[1]],Expanse = Expanse)
+    timecluster <- clusterdf[,2] %>% kmeans(timebins)
+    clustercounts <- timecluster[[1]] %>% table()
 
-    #Runs linear model to perform Anova by a group such as Day of Week.
-    lmresults <-  lm(commutedf[,1] ~ commutedf[,4])
-    anovaresults <- lmresults %>% anova()
+    #this is the initial user specified number of time bins for average lowest time
+    #in case the assumptions for anova fail
+    commutedf <- commutedf %>% dplyr::mutate(Cluster = timecluster[[1]],Expanse = Expanse)
 
     #This will retrieve time of departure cluster
     #which has the lowest average drive time.
@@ -42,42 +44,152 @@ commuteanova <- function(commutedf,Leave ="Left",Arrive="ArriveDesk",Time ="Tota
     maxdepart <- maxdepart[,3]
 
 
-    #The series of if statements begins by
-    #checking for non-significant ANOVA results.
-    if(anovaresults[1,5] > .05){
-      paste("Your commutes are not significantly different between one another using ",timebins," clusters.",sep="") %>%
-        print()
-      #It sill tells minimum average drive time day.
-      paste("Leave between ",mindepart," and ",maxdepart," though for the shortest average drive time.",sep="") %>%
-        print()
-    }else{
-      #If the result is significant, then it runs Tukey's Post-Hoc test, and
-      #it also displays which days were different pairwise.
-      aovresults <-  aov(commutedf[,1] ~ as.factor(commutedf[,4]))
-      posthoc <- TukeyHSD(aovresults)
-      tukeydf <- posthoc[[1]] %>% data.frame()
+    #adding in a kolmogorov smirnov test for normality of clusters
+    ksres <- list()
+    #ks test on multiple groups with list output
+    for(i in 1:length(names(clustercounts))){
+    datcluster <- dplyr::filter(commutedf,Cluster==i)
+    ksres[[i]]<- datcluster[,1] %>%
+      ks.test(y="pnorm",mean(datcluster[,1],sd(datcluster[,1])))
+    #end ks loop
+    }
 
-      #uses select from tidyverse to find significant pairwise
-      #results, eliminated a very large sequence of if else statements
-      #with these 5 lines, which could arguably be one or two lines
-      sig <- tukeydf %>%
-        tibble::rownames_to_column('group') %>%
-        dplyr::filter(p.adj < .05) %>%
-        tibble::column_to_rownames('group') %>%
-        rownames()
+   #logic check for results of kolmogorov-smirnov
+    if(any(lapply(ksres,'[[',2) %>% as.vector() < .05)){
+      #log the drive times if they fail normality in ks
+      commutedf <- commutedf %>% dplyr::mutate(logTime = log(driveTime))
+      kslog <- list()
 
-      paste("Your commutes are significantly different between these time clusters:",sep="") %>%
-        print()
+    #ks log loop
+      for(i in 1:length(names(clustercounts))){
+        datcluster <- dplyr::filter(commutedf,Cluster==i)
+        kslog[[i]]<- datcluster[,6] %>%
+          ks.test(y="pnorm",mean(datcluster[,6],sd(datcluster[,6])))
+        #end ks log loop
+      }
 
-      sig %>% print()
+      #checking the ks on the logs for normality
+      if(any(lapply(kslog,'[[',2) %>% as.vector() < .05)){
+        paste("Leave between ",mindepart," and ",maxdepart," for the shortest average drive time based on descriptive statistics.",sep="") %>%
+          print()
+        print("Your commutes are not normally distirbuted under log transformation.")
+            }else{
+              #testing for heteroscedasticity now
+          if(fligner.test(logTime ~ Cluster, data=commutedf)$p.value < .05){
+            paste("Leave between ",mindepart," and ",maxdepart," for the shortest average drive time based on descriptive statistics.",sep="") %>%
+              print()
+            print("Your commutes are heteroscedastic under log transformation.")
+            #if data is homoscedastic the rest of the analysis follows on the logs
+          }else{
+            #Runs linear model to perform Anova by cluster.
+            lmresults <-  lm(commutedf[,1] ~ commutedf[,4])
+            anovaresults <- lmresults %>% anova()
 
-      paste("Leave between ",mindepart," and ",maxdepart," for the shortest average drive time.",sep="") %>%
-        print()
+
+            #The series of if statements begins by
+            #checking for non-significant ANOVA results.
+            if(anovaresults[1,5] > .05){
+              paste("Your commutes are not significantly different between one another using ",timebins," clusters.",sep="") %>%
+                print()
+              #It sill tells minimum average drive time day.
+              paste("Leave between ",mindepart," and ",maxdepart," though for the shortest average drive time.",sep="") %>%
+                print()
+            }else{
+              #Runs linear model to perform Anova by cluster.
+              lmresults <-  lm(commutedf[,6] ~ commutedf[,4])
+              anovaresults <- lmresults %>% anova()
+
+
+              #The series of if statements begins by
+              #checking for non-significant ANOVA results.
+              if(anovaresults[1,5] > .05){
+                paste("Your commutes are not significantly different between one another using ",timebins," clusters.",sep="") %>%
+                  print()
+                #It sill tells minimum average drive time day.
+                paste("Leave between ",mindepart," and ",maxdepart," though for the shortest average drive time.",sep="") %>%
+                  print()
+              }else{
+
+              #this is on the logged data after failure of ks test for normality
+              #If the result is significant, then it runs Tukey's Post-Hoc test, and
+              #it also displays which times were different pairwise.
+              aovresults <-  aov(commutedf[,1] ~ as.factor(commutedf[,6]))
+              posthoc <- TukeyHSD(aovresults)
+              tukeydf <- posthoc[[1]] %>% data.frame()
+
+              #uses select from tidyverse to find significant pairwise
+              #results, eliminated a very large sequence of if else statements
+              #with these 5 lines, which could arguably be one or two lines
+              sig <- tukeydf %>%
+                tibble::rownames_to_column('group') %>%
+                dplyr::filter(p.adj < .05) %>%
+                tibble::column_to_rownames('group') %>%
+                rownames()
+
+              paste("Your commutes are significantly different between these time clusters:",sep="") %>%
+                print()
+
+              sig %>% print()
+
+              paste("Leave between ",mindepart," and ",maxdepart," for the shortest average drive time.",sep="") %>%
+                print()
+            }
+          }
+          }
+  #end if ks log fail to rejct the null
+      }
+#end if any ks reject the null
     }
 
 
-  }
+    if(fligner.test(driveTime ~ Cluster, data=commutedf)$p.value < .05){
+      paste("Leave between ",mindepart," and ",maxdepart," for the shortest average drive time based on descriptive statistics.",sep="") %>%
+        print()
+      print("Your commutes are heteroscedastic, so no significance tests are performed.")
+    }else{
 
+      #Runs linear model to perform Anova by cluster.
+      lmresults <-  lm(commutedf[,1] ~ commutedf[,4])
+      anovaresults <- lmresults %>% anova()
+
+
+      #The series of if statements begins by
+      #checking for non-significant ANOVA results.
+      if(anovaresults[1,5] > .05){
+        paste("Your commutes are not significantly different between one another using ",timebins," clusters.",sep="") %>%
+          print()
+        #It sill tells minimum average drive time day.
+        paste("Leave between ",mindepart," and ",maxdepart," though for the shortest average drive time.",sep="") %>%
+          print()
+      }else{
+        #If the result is significant, then it runs Tukey's Post-Hoc test, and
+        #it also displays which days were different pairwise.
+        aovresults <-  aov(commutedf[,1] ~ as.factor(commutedf[,4]))
+        posthoc <- TukeyHSD(aovresults)
+        tukeydf <- posthoc[[1]] %>% data.frame()
+
+        #uses select from tidyverse to find significant pairwise
+        #results, eliminated a very large sequence of if else statements
+        #with these 5 lines, which could arguably be one or two lines
+        sig <- tukeydf %>%
+          tibble::rownames_to_column('group') %>%
+          dplyr::filter(p.adj < .05) %>%
+          tibble::column_to_rownames('group') %>%
+          rownames()
+
+        paste("Your commutes are significantly different between these time clusters:",sep="") %>%
+          print()
+
+        sig %>% print()
+
+        paste("Leave between ",mindepart," and ",maxdepart," for the shortest average drive time.",sep="") %>%
+          print()
+      }
+
+
+    }
+#end if statement for using real times, i.e. left and arrival not missing
+  }
 
   #Runs linear model to perform Anova by a group such as Day of Week.
   lmresults <-  lm(commutedf[,1] ~ commutedf[,Group])
